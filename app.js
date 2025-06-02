54f2fbe7eb5d6c7eef2d6e8ea6c7ae4caf33aea7
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-06-02 16:19:39 +0300

    Set required paths for project, enable several middlewares

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+const path = require("node:path");
 const express = require("express");
-
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
