b334ba04568b522f3bc1183734b2a99f3cfd7192
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-06-06 03:36:56 +0300

    Handle sessions, utilize prisma for session storage, passport for auth

--- app.js
+++ app.js
@@ -12,6 +12,76 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
+// Session handlers
+
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const db = require("./prisma/queries");
+const bcrypt = require("bcryptjs");
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+    }),
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await db.getUserByName(username);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect credentials" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return done(null, false, { message: "Incorrect credentials" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getUserById(id);
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
+});
+
 // Routers
 const authRouter = require("./routers/authRouter");
 const fileRouter = require("./routers/fileRouter");
